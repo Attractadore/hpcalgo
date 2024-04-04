63c3c0ee945911f8f8520f813b4c4736e7fabfc6
Michael Bargatin <attractadore02@gmail.com>
2024-04-05 00:55:38 +0200

    SYCL: optimize scan scratch allocation

--- sycl/syclalgo.cpp
+++ sycl/syclalgo.cpp
@@ -1,4 +1,5 @@
 #include "syclalgo.hpp"
+#include <thread>
 #include <utility>
 
 namespace syclalgo {
@@ -45,26 +46,21 @@ enum class ScanType {
   Inclusive,
 };
 
+constexpr int SCAN_WG_SIZE = 64;
+constexpr int SCAN_WI_ELEMS = 8;
+constexpr int SCAN_WG_ELEMS = SCAN_WG_SIZE * SCAN_WI_ELEMS;
+
 template <ScanType ST>
-auto scan(sycl::queue &q, size_t n, const int *d_data, int *d_out,
-          std::span<const sycl::event> dependences = {}) -> sycl::event {
-  constexpr size_t WG_SIZE = 128;
-  constexpr size_t WI_ELEMS = 4;
-  constexpr size_t WG_ELEMS = WG_SIZE * WI_ELEMS;
+auto scan_impl(sycl::queue &q, int n, const int *d_data, int *d_out,
+               int *d_scratch, std::span<const sycl::event> dependences = {})
+    -> sycl::event {
+  int num_groups = ceil_div(n, SCAN_WG_ELEMS);
+  sycl::nd_range<1> range = {num_groups * SCAN_WG_SIZE, SCAN_WG_SIZE};
 
-  size_t num_groups = ceil_div(n, WG_ELEMS);
-  if (num_groups == 0) {
-    return {};
-  }
-  sycl::nd_range<1> range = {num_groups * WG_SIZE, WG_SIZE};
-
-  int *d_block_sum = nullptr;
-  if (num_groups > 1) {
-    d_block_sum = sycl::malloc_device<int>(num_groups, q);
-  }
+  int *d_block_sum = num_groups > 1 ? d_scratch : nullptr;
 
   sycl::event e = q.submit([&](sycl::handler &cg) {
-    sycl::local_accessor<int> shm(WG_ELEMS * 2, cg);
+    sycl::local_accessor<int> shm(SCAN_WG_ELEMS * 2, cg);
 
     kernel_dependences.assign(dependences.begin(), dependences.end());
     cg.depends_on(kernel_dependences);
@@ -72,13 +68,15 @@ auto scan(sycl::queue &q, size_t n, const int *d_data, int *d_out,
 
     cg.parallel_for(range, [=](sycl::nd_item<1> id) {
       sycl::local_ptr<int> shm_src(shm);
-      sycl::local_ptr<int> shm_dst = shm_src + WG_ELEMS;
+      sycl::local_ptr<int> shm_dst = shm_src + SCAN_WG_ELEMS;
 
       auto g = id.get_group();
+      int lid = id.get_local_id();
+      int gid = g.get_group_id();
 
-      for (int i = 0; i < WI_ELEMS; ++i) {
-        int lidx = i * WG_SIZE + id.get_local_id();
-        int gidx = g.get_group_id() * WG_ELEMS + lidx;
+      for (int i = 0; i < SCAN_WI_ELEMS; ++i) {
+        int lidx = i * SCAN_WG_SIZE + lid;
+        int gidx = gid * SCAN_WG_ELEMS + lidx;
         if constexpr (ST == ScanType::Exclusive) {
           gidx--;
           shm_src[lidx] = gidx >= 0 && gidx < n ? d_data[gidx] : 0;
@@ -88,9 +86,9 @@ auto scan(sycl::queue &q, size_t n, const int *d_data, int *d_out,
       }
       sycl::group_barrier(g);
 
-      for (int stride = 1; stride < WG_ELEMS; stride *= 2) {
-        for (int i = 0; i < WI_ELEMS; ++i) {
-          int dst = WG_ELEMS - (i * WG_SIZE + id.get_local_id()) - 1;
+      for (int stride = 1; stride < SCAN_WG_ELEMS; stride *= 2) {
+        for (int i = 0; i < SCAN_WI_ELEMS; ++i) {
+          int dst = SCAN_WG_ELEMS - (i * SCAN_WG_SIZE + lid) - 1;
           int value = shm_src[dst];
           int src = dst - stride;
           if (src >= 0) {
@@ -102,16 +100,16 @@ auto scan(sycl::queue &q, size_t n, const int *d_data, int *d_out,
         std::swap(shm_src, shm_dst);
       }
 
-      for (int i = 0; i < WI_ELEMS; ++i) {
-        int lidx = i * WG_SIZE + id.get_local_id();
-        int gidx = g.get_group_id() * WG_ELEMS + lidx;
+      for (int i = 0; i < SCAN_WI_ELEMS; ++i) {
+        int lidx = i * SCAN_WG_SIZE + lid;
+        int gidx = gid * SCAN_WG_ELEMS + lidx;
         if (gidx < n) {
           d_out[gidx] = shm_src[lidx];
         }
       }
 
       if (d_block_sum && g.leader()) {
-        d_block_sum[g.get_group_id()] = shm_src[WG_ELEMS - 1];
+        d_block_sum[gid] = shm_src[SCAN_WG_ELEMS - 1];
       }
     });
   });
@@ -120,24 +118,49 @@ auto scan(sycl::queue &q, size_t n, const int *d_data, int *d_out,
     return e;
   }
 
-  e = scan<ScanType::Exclusive>(q, num_groups, d_block_sum, d_block_sum,
-                                {&e, 1});
+  e = scan_impl<ScanType::Exclusive>(q, num_groups, d_block_sum, d_block_sum,
+                                     d_scratch + num_groups, {&e, 1});
 
-  e = q.submit([&](sycl::handler &cg) {
+  return q.submit([&](sycl::handler &cg) {
     cg.depends_on(e);
     cg.parallel_for(range, [=](sycl::nd_item<1> id) {
       auto g = id.get_group();
-      for (int i = 0; i < WI_ELEMS; ++i) {
-        int lidx = i * WG_SIZE + g.get_local_id();
-        int gidx = g.get_group_id() * WG_ELEMS + lidx;
+      int lid = id.get_local_id();
+      int gid = g.get_group_id();
+      for (int i = 0; i < SCAN_WI_ELEMS; ++i) {
+        int lidx = i * SCAN_WG_SIZE + lid;
+        int gidx = gid * SCAN_WG_ELEMS + lidx;
         if (gidx < n) {
-          d_out[gidx] += d_block_sum[g.get_group_id()];
+          d_out[gidx] += d_block_sum[gid];
         }
       }
     });
   });
+}
 
-  sycl::free(d_block_sum, q);
+template <ScanType ST>
+auto scan(sycl::queue &q, size_t n, const int *d_data, int *d_out,
+          std::span<const sycl::event> dependences = {}) -> sycl::event {
+  if (n == 0) {
+    return {};
+  }
+
+  size_t scratch_cnt = 0;
+  size_t num_groups = n;
+  do {
+    num_groups = ceil_div(num_groups, SCAN_WG_ELEMS);
+    scratch_cnt += num_groups;
+  } while (num_groups > 1);
+
+  int *d_scratch =
+      scratch_cnt > 1 ? sycl::malloc_device<int>(scratch_cnt, q) : nullptr;
+
+  sycl::event e = scan_impl<ST>(q, n, d_data, d_out, d_scratch, dependences);
+
+  std::thread([q, e, d_scratch]() mutable {
+    e.wait();
+    sycl::free(d_scratch, q);
+  }).detach();
 
   return e;
 }
